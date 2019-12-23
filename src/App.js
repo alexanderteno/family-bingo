963286f011d2f8aa0a154dfb0213e84ceb53ae79
Alexander Teno <alexander.teno@gmail.com>
2019-12-22 20:57:53 -0500

    More stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import './App.css';
-
-const BingoCard = () => {
-
-  return [0, 1, 2, 3, 4].map((i) => {
-    return [0, 1, 2, 3, 4].map((j) => (<div>({i}, {j})</div>));
-  });
-
-}
+import BingoCard from './Components/BingoCard';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Teno Family Bingo
-      </header>
+    <div className="app">
+      <h1 className="header">
+        Teno/Perry Family Bingo
+      </h1>
       <BingoCard />
     </div>
   );
